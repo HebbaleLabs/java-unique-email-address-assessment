1b3705e3a1b62e67770bae500b83ed2864b3b08c
Harsha Bhargav <hbhargav@users.noreply.github.com>
2019-02-27 18:03:35 +0530

    Update UniqueEmailAddress.java

--- src/main/java/com/talfinder/assessment/UniqueEmailAddress.java
+++ src/main/java/com/talfinder/assessment/UniqueEmailAddress.java
@@ -2,8 +2,19 @@ package com.talfinder.assessment;
 
 public class UniqueEmailAddress {
 
-  public int numUniqueEmails(String[] emails) {
-    return 0;
-  }
-
+    public static int numUniqueEmails(String[] emails) {
+        /*
+         * NOTE:
+         * 1. Write your solution within this method
+         * 2. Do not make changes to the Class Name, Method Name or Method Signature
+         * 3. Use the Run-Java command to run main method
+         * 4. Use the BuildAndRunTests command to evaluate your solution
+         */
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        String[] emails = new String[]{"h.bhargav@mailinator.com", "h.bhargav+a1@mailinator.com", "bhargav@mailinator.com"};
+        System.out.println("Unique Email Count : " + numUniqueEmails(emails));
+    }
 }
